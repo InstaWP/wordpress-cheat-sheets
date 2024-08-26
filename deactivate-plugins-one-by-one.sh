fb195bd415ec4cf33936f6226f4a4428da9e0754
vikasiwp <108919165+vikasiwp@users.noreply.github.com>
2024-08-26 15:34:53 +0530

    Update deactivate-plugins-one-by-one.sh

--- deactivate-plugins-one-by-one.sh
+++ deactivate-plugins-one-by-one.sh
@@ -22,21 +22,35 @@
 # Define WP-CLI options for skipping plugins and themes
 WP_CLI_OPTIONS="--skip-plugins --skip-themes"
 
+# Skip certain plugins
+SKIP_PLUGINS="woocommerce,wordpress-seo"
+
 # Get a list of all active plugins, excluding WooCommerce
-active_plugins=$(wp plugin list --status=active --field=name $WP_CLI_OPTIONS | grep -v 'woocommerce')
+active_plugins=$(wp $WP_CLI_OPTIONS plugin list --status=active --field=name  | grep -vE "$(echo $SKIP_PLUGINS | sed 's/,/|/g')")
+# Count the number of active plugins (excluding WooCommerce)
+plugin_count=$(echo "$active_plugins" | grep -v '^$' | wc -l)
+
+# Output the number of plugins found
+echo "Number of active plugins (excluding WooCommerce): $plugin_count"
+
 
 # Loop through each active plugin
 for plugin in $active_plugins; do
+    # Skip empty plugin names
+    if [ -z "$plugin" ]; then
+        continue
+    fi
+
     # Deactivate the plugin
     echo "Deactivating plugin: $plugin"
-    wp plugin deactivate $plugin $WP_CLI_OPTIONS
+    wp $WP_CLI_OPTIONS plugin deactivate $plugin 
 
     # Wait for user input before proceeding
     read -p "Plugin $plugin deactivated. Press [Enter] to reactivate and proceed to the next plugin..."
 
     # Reactivate the plugin
     echo "Reactivating plugin: $plugin"
-    wp plugin activate $plugin $WP_CLI_OPTIONS
+    wp $WP_CLI_OPTIONS plugin activate $plugin $WP_CLI_OPTIONS
     echo
 done
 
